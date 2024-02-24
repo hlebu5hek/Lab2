edc6941dc26c2917d73294a702d89e826e5173b0
hlebu5hek <74304719+hlebu5hek@users.noreply.github.com>
2024-02-24 12:18:37 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+'''
+Натуральные числа.
+Выводит на экран нечетные числа, содержащие четное количество цифр, превышающее К.
+Список используемых цифр (прописью) и их количество выводится отдельно.
+'''
+
+import re
+
+def check_num(gived_num, k):
+    if not re.match("^[0-9]+$", gived_num):
+        return False
+
+    if not re.match("^[13579]+$", gived_num[-1]):
+        return False
+
+    if len(gived_num) % 2 != 0:
+        return False
+
+    if len(gived_num) <= k:
+        return False
+
+    return True
+
+
+dc_cifr = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
+output_arr = []
+
+k = int(input('Число К: \n'))
+
+gived_num = '1'
+
+choice = input('Как вводятся лексемы:\n1) Вручную; \n2) Из файла; \n').lower()
+
+if choice in ['1', 'one', 'hand', 'вручную']:
+    while gived_num:
+        gived_num = input('Число из потока: \n')
+
+        if gived_num in ['0', '000', 'exit', 'stop', 'cancel', 'out']:
+            break
+
+        if(check_num(gived_num, k)):
+            output_arr.append(gived_num)
+
+elif choice in ['2', 'two', 'file', 'из файла']:
+    file_name = input('Имя файла:\n')
+
+    try:
+        open(file_name, 'r')
+    except:
+        print('Файл отсутствует в директории проекта')
+        exit()
+
+    with open(file_name, 'r') as file:
+        while 1:
+            gived_num = file.readline().replace('\n', '')
+            if not gived_num:
+                #print('Файл закончился')
+                break
+
+            if (check_num(gived_num, k)):
+                output_arr.append(gived_num)
+
+for output_num in output_arr:
+    print(output_num)
+
+    cifr = []
+
+    for i in output_num:
+        if i not in cifr:
+            cifr.append(i)
+
+    cifr = sorted(cifr, key= lambda x: int(x))
+
+    print('Количество использованных цифр:')
+    print(len(cifr))
+
+    cifr_propis = list(map(lambda x: dc_cifr[x], cifr))
+
+    print('Использованные цифры:')
+    print(*cifr_propis, sep=', ')
+
+    print('___________________\n')
