2a8c3628dc37f2fc5d4c51920738d93b7d0c3027
hlebu5hek <gleb.mikhailow2017@yandex.ru>
2024-02-24 14:28:29 +0400

    short

--- main.py
+++ main.py
@@ -8,10 +8,9 @@
 import re
 dc_cifr = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'} #Словарь цифр
 k = int(input("Число К = ")) #Число К
-buffer = '1' #Баффер
 with open("text.txt", 'r') as file:
+    buffer = file.readline().replace('\n', '') #Баффер
     while buffer:
-        buffer = file.readline().replace('\n', '')
         if re.match("^[0-9]+$", buffer) and re.match("^[13579]+$", buffer[-1]) and (len(buffer) % 2 == 0) and (len(buffer) > k):
             print('\n', buffer, sep=' ')
             cifr = []
@@ -20,3 +19,4 @@ with open("text.txt", 'r') as file:
                     cifr.append(i)
             cifr_propis = list(map(lambda x: dc_cifr[x], sorted(cifr, key=lambda x: int(x))))
             print(' Количество использованных цифр:\n', len(cifr), '\n', 'Использованные цифры:\n', *cifr_propis, sep=' ')
+            buffer = file.readline().replace('\n', '')
